f678d90c745661c03377f86ac95dc9683aa8576e
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-11 22:44:03 +0200

    feat: add rofi and gtk3

--- hosts/virtualbox-vm/configuration.nix
+++ hosts/virtualbox-vm/configuration.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   imports = [
     ../../modules/common.nix
@@ -17,20 +16,43 @@
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.dragAndDrop = true;
   virtualisation.virtualbox.guest.clipboard = true;
+ 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 
   programs.hyprland = { 
     enable = true;
     xwayland.enable = true;
+  };
 
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
   };
 
   environment.systemPackages = with pkgs; [ 
+    gtk3
     mako
     wezterm
     kitty
     foot
-    waybar
     hyprpaper
+    rofi
+    (pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+     })
+    )
   ];
 
   system.stateVersion = "25.05";
