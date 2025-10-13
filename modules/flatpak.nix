859b472682dc63e4816163ef77162bbcb4421789
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-13 19:51:22 +0200

    attempt at installing more flatpak packages

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -6,11 +6,19 @@
     
     update = {
       auto.enable = false;
+      onActivation = true;
     };
+
     
     packages = [
       # GUI to manage flatpak apps and permissions
       "com.github.tchx84.Flatseal"
+      "com.disordapp.Discord"
+      # "org.signal.Signal"
+      # "org.keepassxc.KeePassXC"
+      # "md.obsidian.Obsidian"
+      # "me.proton.Mail"
+      # "com.protonvpn.www"
     ];
   };
 }
