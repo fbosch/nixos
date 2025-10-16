db3593d9fbae729ac7eec2163cfdc3e74702add0
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-16 22:32:37 +0200

    attempt to fix icon theme

--- modules/win11-icons.nix
+++ modules/win11-icons.nix
@@ -14,21 +14,24 @@ let
 
     nativeBuildInputs = [ pkgs.gtk3 ];
 
+    dontBuild = true;
     dontDropIconThemeCache = true;
 
     installPhase = ''
       runHook preInstall
       
-      patchShebangs install.sh
-      
       mkdir -p $out/share/icons
       
-      # Install default theme
-      name=Win11 ./install.sh -d $out/share/icons -t default
+      # Copy the icon theme directories directly
+      cp -r Win11* $out/share/icons/
       
-      # Remove broken symlinks and caches
+      # Remove any broken symlinks
       find $out -xtype l -delete
-      find $out -name "icon-theme.cache" -delete
+      
+      # Update icon cache for each theme
+      for theme in $out/share/icons/Win11*; do
+        ${pkgs.gtk3}/bin/gtk-update-icon-cache -f -t "$theme" 2>/dev/null || true
+      done
       
       runHook postInstall
     '';
