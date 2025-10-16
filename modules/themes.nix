13c1cf453fcbdcb45288841a748bc7de369fa5e7
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-16 05:25:39 +0000

    fix: custom theme install

--- modules/themes.nix
+++ modules/themes.nix
@@ -1,21 +1,25 @@
 { pkgs, lib, ... }:
 
 let
-  mono-gtk-theme = pkgs.stdenv.mkDerivation {
+  mono-gtk-theme = pkgs.stdenv.mkDerivation rec {
     pname = "mono-gtk-theme";
     version = "1.3";
-    
-    src = pkgs.fetchurl {
-      url = "https://github.com/witalihirsch/Mono-gtk-theme/releases/download/1.3/MonoTheme.tar.xz";
-      sha256 = "sha256-QGJgaKf+ODG49+31p6jySvK0gGdgm2/9flhebvjOC78=";
-   };
-    
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/witalihirsch/Mono-gtk-theme/releases/download/${version}/MonoTheme.zip";
+      sha256 = "sha256-/Ysak/WeWY4+svCu3yhi/blfcUsSnGOrWn8/YCyNTYM=";
+      stripRoot = true;
+    };
+
+    dontBuild = true;
+    dontConfigure = true;
+
     installPhase = ''
-      mkdir -p $out/share/themes
-      cp -r MonoTheme-1.3 $out/share/themes/
+      	    mkdir -p $out/share/themes
+      	    cp -r . $out/share/themes/MonoTheme-${version}
     '';
-    
-    meta = with pkgs.lib; {
+
+    meta = with lib; {
       description = "Mono GTK theme";
       homepage = "https://github.com/witalihirsch/Mono-gtk-theme";
       platforms = platforms.linux;
@@ -24,13 +28,5 @@ let
 in
 {
   home.packages = [ mono-gtk-theme ];
-  
-  gtk = {
-    enable = true;
-    theme = {
-      name = "MonoTheme-1.3";
-      package = mono-gtk-theme;
-    };
-  };
-}
 
+}
