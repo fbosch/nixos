d2d44a3d911e0b6c806b009608f6f87a11206550
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-17 19:37:58 +0200

    refactor fonts module

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,79 +1,126 @@
+{ lib, pkgs, ... }:
 
-{ lib, config, pkgs, ... }:
 let
-  zenbones-mono = pkgs.fetchzip {
-    url = "https://github.com/zenbones-theme/zenbones-mono/releases/download/v2.400/Zenbones-Brainy-TTF.zip";
-    sha256 = "sha256-Wrn9BYNs0Z9BDau60u2eX/LleXzcH1MuIJph6XfIRTE=";
-    stripRoot = false;
+  mkFont = {
+    src,
+    name,
+    description,
+    installPhase ? null
+  }:
+  let
+    font = pkgs.stdenv.mkDerivation {
+      inherit name src;
+      
+      dontBuild = true;
+      dontConfigure = true;
+      
+      installPhase = if installPhase != null then installPhase else ''
+        runHook preInstall
+        
+        mkdir -p $out/share/fonts
+        find $src -name '*.ttf' -exec cp {} $out/share/fonts \;
+        find $src -name '*.otf' -exec cp {} $out/share/fonts \;
+        
+        runHook postInstall
+      '';
+      
+      meta = with lib; {
+        inherit description;
+        platforms = platforms.linux;
+      };
+    };
+  in {
+    derivation = font;
+    homeFile = {
+      ".local/share/fonts/${name}".source = "${font}/share/fonts";
+    };
   };
-  babelstone-elder-futhark = pkgs.fetchurl {
-    url = "https://babelstone.co.uk/Fonts/Download/BabelStoneRunicElderFuthark.ttf";
-    sha256 = "sha256-awYvgb6O07ouxwqg2OgomDia1j4jmVFwyAr7oSacNws=";
-  };
-  tahoma = pkgs.fetchurl {
-    url = "https://gitlab.winehq.org/wine/wine/-/raw/master/fonts/tahoma.ttf?ref_type=heads&inline=false";
-    sha256 = "sha256-kPGrrU2gzgPaXSJ37nWpYAzoEtN8kOq3bgg4/6eTflU=";
-  };
-  apple-emojis = pkgs.fetchurl {
-    url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v18.4/AppleColorEmoji.ttf";
-    sha256 = "sha256-pP0He9EUN7SUDYzwj0CE4e39SuNZ+SVz7FdmUviF6r0=";
-  };
-  phosphor-fill = pkgs.fetchurl {
-    url = "https://github.com/phosphor-icons/web/blob/master/src/fill/Phosphor-Fill.ttf";
-    sha256 = "sha256-x90bxwqKW5MfVTDliiBGeh515xwhigS4BGZkgzejHWs=";
-  };
+
+  mkFontFromZip = {
+    url,
+    name,
+    description,
+    sha256 ? lib.fakeSha256,
+    stripRoot ? true,
+    installPhase ? null
+  }:
+    mkFont {
+      inherit name description installPhase;
+      src = pkgs.fetchzip {
+        inherit url sha256 stripRoot;
+      };
+    };
+
+  mkFontFromUrl = {
+    url,
+    name,
+    fileName,
+    description,
+    sha256 ? lib.fakeSha256
+  }:
+    mkFont {
+      inherit name description;
+      src = pkgs.fetchurl {
+        inherit url sha256;
+      };
+      installPhase = ''
+        runHook preInstall
+        
+        mkdir -p $out/share/fonts
+        cp $src $out/share/fonts/${fileName}
+        
+        runHook postInstall
+      '';
+    };
+
+  fonts = [
+    (mkFontFromZip {
+      url = "https://github.com/zenbones-theme/zenbones-mono/releases/download/v2.400/Zenbones-Brainy-TTF.zip";
+      name = "zenbones-mono";
+      description = "Zenbones Mono font family";
+      sha256 = "sha256-Wrn9BYNs0Z9BDau60u2eX/LleXzcH1MuIJph6XfIRTE=";
+      stripRoot = false;
+    })
+    (mkFontFromUrl {
+      url = "https://babelstone.co.uk/Fonts/Download/BabelStoneRunicElderFuthark.ttf";
+      name = "babelstone-elder-futhark";
+      fileName = "BabelStoneRunicElderFuthark.ttf";
+      description = "BabelStone Runic Elder Futhark font";
+      sha256 = "sha256-awYvgb6O07ouxwqg2OgomDia1j4jmVFwyAr7oSacNws=";
+    })
+    (mkFontFromUrl {
+      url = "https://gitlab.winehq.org/wine/wine/-/raw/master/fonts/tahoma.ttf?ref_type=heads&inline=false";
+      name = "tahoma";
+      fileName = "tahoma.ttf";
+      description = "Tahoma font";
+      sha256 = "sha256-kPGrrU2gzgPaXSJ37nWpYAzoEtN8kOq3bgg4/6eTflU=";
+    })
+    (mkFontFromUrl {
+      url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v18.4/AppleColorEmoji.ttf";
+      name = "apple-color-emoji";
+      fileName = "AppleColorEmoji.ttf";
+      description = "Apple Color Emoji font";
+      sha256 = "sha256-pP0He9EUN7SUDYzwj0CE4e39SuNZ+SVz7FdmUviF6r0=";
+    })
+    (mkFontFromUrl {
+      url = "https://github.com/phosphor-icons/web/blob/master/src/fill/Phosphor-Fill.ttf";
+      name = "phosphor-fill";
+      fileName = "Phosphor-Fill.ttf";
+      description = "Phosphor Fill icon font";
+      sha256 = "sha256-x90bxwqKW5MfVTDliiBGeh515xwhigS4BGZkgzejHWs=";
+    })
+  ];
+  
+  fontHomeFiles = lib.mkMerge (map (f: f.homeFile) fonts);
 in
 {
-  fonts.fontDir.enable = true;
-  fonts.fontconfig.useEmbeddedBitmaps = true;
+  fonts.fontconfig.enable = true;
+  
   fonts.packages = with pkgs; [
     nerd-fonts.symbols-only
     nerd-fonts.jetbrains-mono
     font-awesome
-    (pkgs.stdenv.mkDerivation {
-      name = "zenbones-mono";
-      src = zenbones-mono;
-      installPhase = ''
-        mkdir -p $out/share/fonts/truetype
-        find $src -name '*.ttf' -exec cp {} $out/share/fonts/truetype \;
-        find $src -name '*.otf' -exec cp {} $out/share/fonts/truetype \;
-      '';
-    })
-    (pkgs.stdenv.mkDerivation {
-      name = "babelstone-elder-futhark";
-      src = babelstone-elder-futhark;
-      dontUnpack = true;
-      installPhase = ''
-        mkdir -p $out/share/fonts/truetype
-        cp $src $out/share/fonts/truetype/BabelStoneRunicElderFuthark.ttf
-      '';
-    })
-    (pkgs.stdenv.mkDerivation {
-      name = "tahoma";
-      src = tahoma;
-      dontUnpack = true;
-      installPhase = ''
-        mkdir -p $out/share/fonts/truetype
-        cp $src $out/share/fonts/truetype/tahoma.ttf
-      '';
-    })
-    (pkgs.stdenv.mkDerivation {
-      name = "apple-color-emoji";
-      src = apple-emojis;
-      dontUnpack = true;
-      installPhase = ''
-        mkdir -p $out/share/fonts/truetype
-        cp $src $out/share/fonts/truetype/AppleColorEmoji.ttf
-      '';
-    })
-    (pkgs.stdenv.mkDerivation {
-      name = "Phosphor-Fill";
-      src = phosphor-fill;
-      dontUnpack = true;
-      installPhase = ''
-        mkdir -p $out/share/fonts/truetype
-        cp $src $out/share/fonts/truetype/Phosphor-Fill.ttf
-      '';
-    })
   ];
+  
+  home.file = fontHomeFiles;
 }
