067609865599ae7c7b282cc219c9a0e6abd554a2
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-29 23:26:22 +0100

    chore: cleanup

--- modules/hosts/rvn-vm.nix
+++ modules/hosts/rvn-vm.nix
@@ -2,31 +2,28 @@
 
 {
   flake.modules.nixos."hosts/rvn-vm" = {
-    imports = [
-      # Base system modules from dendritic tree
-      config.flake.modules.nixos.base
-      config.flake.modules.nixos.i18n
-      config.flake.modules.nixos.vpn
-      config.flake.modules.nixos.packages
-      config.flake.modules.nixos.security
-      config.flake.modules.nixos.fonts
-      config.flake.modules.nixos.hyprland
-      config.flake.modules.nixos.audio
+    imports = with config.flake.modules.nixos; [
+      base
+      i18n
+      vpn
+      packages
+      security
+      fonts
+      hyprland
+      audio
       
-      # Machine-specific configuration (auto-generated on install)
       ../../machines/virtualbox-vm/configuration.nix
       ../../machines/virtualbox-vm/hardware-configuration.nix
       
-      # Home Manager integration
       {
         home-manager.users.fbb = {
-          imports = [
-            config.flake.modules.homeManager.base
-            config.flake.modules.homeManager.dotfiles
-            config.flake.modules.homeManager.programs
-            config.flake.modules.homeManager.flatpak
-            config.flake.modules.homeManager.fonts
-            config.flake.modules.homeManager.gtk
+          imports = with config.flake.modules.homeManager; [
+            base
+            dotfiles
+            programs
+            flatpak
+            fonts
+            gtk
             inputs.flatpaks.homeManagerModules.nix-flatpak
             inputs.vicinae.homeManagerModules.default
           ];
