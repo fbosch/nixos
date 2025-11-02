fd7cd19130f0ebe5c316b0d8520ed09081be8b8a
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-11-02 22:06:26 +0100

    chore: format

--- modules/applications/firejail.nix
+++ modules/applications/firejail.nix
@@ -7,25 +7,28 @@
           executable =
             "${pkgs.local.chromium-protonmail}/bin/chromium-protonmail";
           profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop = "${pkgs.local.chromium-protonmail}/share/applications/chromium-protonmail.desktop";
+          desktop =
+            "${pkgs.local.chromium-protonmail}/share/applications/chromium-protonmail.desktop";
         };
         chromium-protoncalendar = {
           executable =
             "${pkgs.local.chromium-protoncalendar}/bin/chromium-protoncalendar";
           profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop = "${pkgs.local.chromium-protoncalendar}/share/applications/chromium-protoncalendar.desktop";
+          desktop =
+            "${pkgs.local.chromium-protoncalendar}/share/applications/chromium-protoncalendar.desktop";
         };
         chromium-youtubemusic = {
           executable =
             "${pkgs.local.chromium-youtubemusic}/bin/chromium-youtubemusic";
           profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop = "${pkgs.local.chromium-youtubemusic}/share/applications/chromium-youtubemusic.desktop";
+          desktop =
+            "${pkgs.local.chromium-youtubemusic}/share/applications/chromium-youtubemusic.desktop";
         };
         chromium-notion = {
-          executable =
-            "${pkgs.local.chromium-notion}/bin/chromium-notion";
+          executable = "${pkgs.local.chromium-notion}/bin/chromium-notion";
           profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop = "${pkgs.local.chromium-notion}/share/applications/chromium-notion.desktop";
+          desktop =
+            "${pkgs.local.chromium-notion}/share/applications/chromium-notion.desktop";
         };
       };
     };
