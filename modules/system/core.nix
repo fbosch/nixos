e849d65d8922aed5f51a61de3572a2d6940719bf
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-11-06 08:27:54 +0100

    feat: enable gc

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -12,12 +12,23 @@
 
     nixpkgs.config.allowUnfree = true;
 
-    nix.settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [ "root" "@wheel" ];
+    nix = {
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        trusted-users = [ "root" "@wheel" ];
+        auto-optimise-store = true;
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 14d";
+      };
+
+      optimise.automatic = true;
     };
 
     home-manager = {
