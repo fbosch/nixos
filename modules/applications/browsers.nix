066219e90ec7058f743ee23465b6d163a8c25059
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-11-09 21:47:23 +0100

    chore: cleanup

--- modules/applications/browsers.nix
+++ modules/applications/browsers.nix
@@ -1,19 +1,5 @@
 {
-  flake.modules.homeManager.applications = { pkgs, ... }:
-    let
-      defaultBrowser = "helium-browser.desktop";
-    in
-    {
-      home.packages = with pkgs; [ local.helium-browser ];
-
-      xdg.mimeApps.defaultApplications = {
-        "text/html" = [ defaultBrowser ];
-        "x-scheme-handler/http" = [ defaultBrowser ];
-        "x-scheme-handler/https" = [ defaultBrowser ];
-        "x-scheme-handler/about" = [ defaultBrowser ];
-        "application/xhtml+xml" = [ defaultBrowser ];
-        "application/x-extension-htm" = [ defaultBrowser ];
-        "application/x-extension-html" = [ defaultBrowser ];
-      };
-    };
+  flake.modules.homeManager.applications = { pkgs, ... }: {
+    home.packages = with pkgs; [ local.helium-browser ];
+  };
 }
