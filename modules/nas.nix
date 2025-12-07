11f1eaac8f8239cd189ba8857d8dc5414040868c
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-07 14:48:19 +0100

    refactor(nas): extract mount configuration into helper functions

--- modules/nas.nix
+++ modules/nas.nix
@@ -1,141 +1,50 @@
 {
-  flake.modules.nixos.nas = { config, meta, ... }: {
-    systemd = {
-      tmpfiles.rules = [
-        "d /mnt/nas 0755 ${meta.user.username} users -"
-        "d /mnt/nas/homes 0755 ${meta.user.username} users -"
-        "d /mnt/nas/music 0755 ${meta.user.username} users -"
-        "d /mnt/nas/photo 0755 ${meta.user.username} users -"
-        "d /mnt/nas/video 0755 ${meta.user.username} users -"
-        "d /mnt/nas/web 0755 ${meta.user.username} users -"
-        "d /mnt/nas/downloads 0755 ${meta.user.username} users -"
-        "d /mnt/nas/cloud-backup 0755 ${meta.user.username} users -"
-        "d /mnt/nas/FrederikDocs 0755 ${meta.user.username} users -"
-        "d /mnt/nas/websites 0755 ${meta.user.username} users -"
+  flake.modules.nixos.nas = { config, lib, meta, ... }:
+    let
+      # List of NAS shares to mount
+      shares = [
+        "homes"
+        "music"
+        "photo"
+        "video"
+        "web"
+        "downloads"
+        "cloud-backup"
+        "FrederikDocs"
+        "websites"
       ];
 
-      mounts = [
-        {
-          type = "cifs";
-          what = "//rvn-nas/homes";
-          where = "/mnt/nas/homes";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/music";
-          where = "/mnt/nas/music";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/photo";
-          where = "/mnt/nas/photo";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/video";
-          where = "/mnt/nas/video";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/web";
-          where = "/mnt/nas/web";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/downloads";
-          where = "/mnt/nas/downloads";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/cloud-backup";
-          where = "/mnt/nas/cloud-backup";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/FrederikDocs";
-          where = "/mnt/nas/FrederikDocs";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-        {
-          type = "cifs";
-          what = "//rvn-nas/websites";
-          where = "/mnt/nas/websites";
-          options = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
-        }
-      ];
+      # Common CIFS mount options
+      cifsOptions = "credentials=${config.sops.templates.smbcredentials.path},uid=${meta.user.username},gid=users,forceuid,forcegid,iocharset=utf8,file_mode=0664,dir_mode=0775,vers=3.0";
 
-      automounts = [
-        {
-          where = "/mnt/nas/homes";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/music";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/photo";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/video";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/web";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/downloads";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/cloud-backup";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/FrederikDocs";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-        {
-          where = "/mnt/nas/websites";
-          wantedBy = [ "multi-user.target" ];
-          automountConfig = {
-            TimeoutIdleSec = "30s";
-          };
-        }
-      ];
+      # Generate tmpfile rule for a share
+      mkTmpfileRule = share: "d /mnt/nas/${share} 0755 ${meta.user.username} users -";
+
+      # Generate mount configuration for a share
+      mkMount = share: {
+        type = "cifs";
+        what = "//rvn-nas/${share}";
+        where = "/mnt/nas/${share}";
+        options = cifsOptions;
+      };
+
+      # Generate automount configuration for a share
+      mkAutomount = share: {
+        where = "/mnt/nas/${share}";
+        wantedBy = [ "multi-user.target" ];
+        automountConfig = {
+          TimeoutIdleSec = "30s";
+        };
+      };
+    in
+    {
+      systemd = {
+        tmpfiles.rules =
+          [ "d /mnt/nas 0755 ${meta.user.username} users -" ]
+          ++ (map mkTmpfileRule shares);
+
+        mounts = map mkMount shares;
+        automounts = map mkAutomount shares;
+      };
     };
-  };
 }
