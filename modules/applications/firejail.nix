6f7c78500f39f01cfc52093e88b12c48f10b2c2f
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-11 16:12:21 +0100

    Make Firejail wrapping automatic for all chromium packages

--- modules/applications/firejail.nix
+++ modules/applications/firejail.nix
@@ -1,36 +1,24 @@
 {
-  flake.modules.nixos.applications = { pkgs, ... }: {
+  flake.modules.nixos.applications = { pkgs, lib, ... }: {
     programs.firejail = {
       enable = true;
-      wrappedBinaries = {
-        chromium-protonmail = {
-          executable =
-            "${pkgs.local.chromium-protonmail}/bin/chromium-protonmail";
-          profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop =
-            "${pkgs.local.chromium-protonmail}/share/applications/chromium-protonmail.desktop";
-        };
-        chromium-protoncalendar = {
-          executable =
-            "${pkgs.local.chromium-protoncalendar}/bin/chromium-protoncalendar";
-          profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop =
-            "${pkgs.local.chromium-protoncalendar}/share/applications/chromium-protoncalendar.desktop";
-        };
-        chromium-youtubemusic = {
-          executable =
-            "${pkgs.local.chromium-youtubemusic}/bin/chromium-youtubemusic";
-          profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop =
-            "${pkgs.local.chromium-youtubemusic}/share/applications/chromium-youtubemusic.desktop";
-        };
-        chromium-notion = {
-          executable = "${pkgs.local.chromium-notion}/bin/chromium-notion";
-          profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          desktop =
-            "${pkgs.local.chromium-notion}/share/applications/chromium-notion.desktop";
-        };
-      };
+      wrappedBinaries =
+        let
+          # Get all chromium packages from pkgs.local
+          chromiumPackages = lib.filterAttrs
+            (name: _: lib.hasPrefix "chromium-" name)
+            pkgs.local;
+        in
+        lib.mapAttrs'
+          (name: package: {
+            inherit name;
+            value = {
+              executable = "${package}/bin/${name}";
+              profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
+              desktop = "${package}/share/applications/${name}.desktop";
+            };
+          })
+          chromiumPackages;
     };
   };
 }
