6acae40676300c8d697283f3693e9027b699a404
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-11 15:49:49 +0100

    Add chromium-realforce package for Realforce Connect

---
+++ pkgs/by-name/chromium-realforce/package.nix
@@ -0,0 +1,18 @@
+{ pkgs }:
+
+pkgs.nix-webapps-lib.mkChromiumApp {
+  appName = "chromium-realforce";
+  categories = [ "Utility" "Network" ];
+  class = "chromium-realforce";
+  desktopName = "Realforce Connect";
+  comment = "Realforce keyboard configuration and firmware update tool";
+  icon = ./realforce-connect.png;
+  profile = "RealforceConnectProfile";
+  url = "https://realforce-connect.online";
+  hardening = {
+    extraFlags = [
+      "--hide-scrollbars"
+      "--user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\""
+    ];
+  };
+}
