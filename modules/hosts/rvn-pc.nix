85031f07cefd8a0c8961b05e98c2d417d4cb31c9
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-23 17:52:54 +0100

    feat: added desktop host

---
+++ modules/hosts/rvn-pc.nix
@@ -0,0 +1,37 @@
+{ inputs, config, ... }:
+
+{
+  flake.modules.nixos."hosts/rvn-pc" = config.flake.lib.mkHost {
+    preset = "desktop";
+
+    hostImports = [
+      ../../machines/desktop/configuration.nix
+      ../../machines/desktop/hardware-configuration.nix
+      ({ pkgs, ... }: {
+        # environment.sessionVariables = {
+        #   GSK_RENDERER = "cairo";
+        #   WLR_RENDERER_ALLOW_SOFTWARE = "1";
+        #   TERMINAL = "foot";
+        # };
+
+        # security.sudo.extraConfig = ''
+        #   Defaults timestamp_timeout = 120
+        # '';
+
+        # environment.systemPackages = [
+        #   pkgs.local.chromium-realforce
+        # ];
+      })
+    ];
+
+    # extraNixos = [ "secrets" "nas" ];
+
+    extraHomeManager = [
+      config.flake.modules.homeManager.dotfiles
+      inputs.flatpaks.homeManagerModules.nix-flatpak
+      inputs.vicinae.homeManagerModules.default
+    ];
+
+    inherit (config.flake.meta.user) username;
+  };
+}
