9b5da518d1573cb0c19e457d95a9d3ed859b8325
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-24 08:00:10 +0100

    feat: set zen browser as default browser

--- modules/applications/browsers.nix
+++ modules/applications/browsers.nix
@@ -5,11 +5,11 @@
       home.packages = with pkgs; [ local.helium-browser ];
 
       xdg.mimeApps.defaultApplications = {
-        "text/html" = [ "helium-browser.desktop" ];
-        "x-scheme-handler/http" = [ "helium-browser.desktop" ];
-        "x-scheme-handler/https" = [ "helium-browser.desktop" ];
-        "x-scheme-handler/about" = [ "helium-browser.desktop" ];
-        "x-scheme-handler/unknown" = [ "helium-browser.desktop" ];
+        "text/html" = [ "io.github.zen_browser.zen.desktop" ];
+        "x-scheme-handler/http" = [ "io.github.zen_browser.zen.desktop" ];
+        "x-scheme-handler/https" = [ "io.github.zen_browser.zen.desktop" ];
+        "x-scheme-handler/about" = [ "io.github.zen_browser.zen.desktop" ];
+        "x-scheme-handler/unknown" = [ "io.github.zen_browser.zen.desktop" ];
       };
     };
 }
--- modules/applications/flatpak.nix
+++ modules/applications/flatpak.nix
@@ -1,64 +1,67 @@
 {
   flake.modules.nixos.applications = _: { services.flatpak.enable = true; };
-  flake.modules.homeManager.applications = { pkgs, ... }: {
-    services.flatpak = {
-      enable = true;
-      uninstallUnmanaged = true;
+  flake.modules.homeManager.applications =
+    { pkgs, ... }:
+    {
+      services.flatpak = {
+        enable = true;
+        uninstallUnmanaged = true;
 
-      overrides = {
-        global = {
-          Context.sockets = [
-            "wayland"
-            "!x11"
-            "!fallback-x11"
-          ];
-          Context.filesystems = [
-            "xdg-config/fontconfig:ro"
-            "~/.local/share/fonts:ro"
-            "/nix/store:ro"
-          ];
+        overrides = {
+          global = {
+            Context.sockets = [
+              "wayland"
+              "!x11"
+              "!fallback-x11"
+            ];
+            Context.filesystems = [
+              "xdg-config/fontconfig:ro"
+              "~/.local/share/fonts:ro"
+              "/nix/store:ro"
+            ];
+          };
         };
-      };
 
-      update = {
-        onActivation = true;
-        auto = {
-          enable = true;
-          onCalendar = "weekly";
+        update = {
+          onActivation = true;
+          auto = {
+            enable = true;
+            onCalendar = "weekly";
+          };
         };
+
+        remotes = [
+          {
+            name = "flathub";
+            location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+          }
+        ];
+
+        packages = [
+          "com.github.tchx84.Flatseal"
+          "io.github.flattool.Warehouse"
+          "com.discordapp.Discord"
+          "org.signal.Signal"
+          "org.keepassxc.KeePassXC"
+          "md.obsidian.Obsidian"
+          "com.usebottles.bottles"
+          "org.gnome.baobab"
+          "org.gnome.FileRoller"
+          "org.gnome.TextEditor"
+          "org.gnome.Decibels"
+          # "org.gnome.World.PikaBackup"
+          "app.zen_browser.zen"
+          "be.alexandervanhee.gradia"
+          "com.plexamp.Plexamp"
+          "re.sonny.OhMySVG"
+          # "dev.zed.Zed"
+          # "com.rustdesk.RustDesk"
+          # "org.localsend.localsend_app"
+          # "io.github.wiiznokes.fan-control"
+          # "tv.plex.PlexDesktop"
+          # "com.todoist.Todoist"
+          # "io.mgba.mGBA"
+        ];
       };
-
-      remotes = [
-        {
-          name = "flathub";
-          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-        }
-      ];
-
-      packages = [
-        "com.github.tchx84.Flatseal"
-        "io.github.flattool.Warehouse"
-        "com.discordapp.Discord"
-        "org.signal.Signal"
-        "org.keepassxc.KeePassXC"
-        "md.obsidian.Obsidian"
-        "com.usebottles.bottles"
-        "org.gnome.baobab"
-        "org.gnome.FileRoller"
-        "org.gnome.TextEditor"
-        "org.gnome.Decibels"
-        # "org.gnome.World.PikaBackup"
-        "be.alexandervanhee.gradia"
-        "com.plexamp.Plexamp"
-        "re.sonny.OhMySVG"
-        # "dev.zed.Zed"
-        # "com.rustdesk.RustDesk"
-        # "org.localsend.localsend_app"
-        # "io.github.wiiznokes.fan-control"
-        # "tv.plex.PlexDesktop"
-        # "com.todoist.Todoist"
-        # "io.mgba.mGBA"
-      ];
     };
-  };
 }
