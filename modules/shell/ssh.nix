30cb1d37fca0e36742c1a5d17d8544d86811ea4d
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-01-23 17:17:25 +0100

    chore: update SSH host configurations to add pc and srv

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -3,21 +3,14 @@ _:
   flake.modules.homeManager.shell = {
     programs.ssh = {
       enable = true;
-
-      # Disable deprecated default config
-      # Set default values explicitly in "*" matchBlock if needed
       enableDefaultConfig = false;
 
       matchBlocks = {
-        # Default settings for all hosts
-        "*" = {
-          # Add any default SSH options you want here
-          # For example:
-          # serverAliveInterval = 60;
-          # serverAliveCountMax = 3;
-        };
-
-        "rvn-srv" = {
+        "pc" = {
+          hostname = "192.168.1.169";
+          user = "fbb";
+        }
+        "srv" = {
           hostname = "192.168.1.46";
           user = "fbb";
         };
