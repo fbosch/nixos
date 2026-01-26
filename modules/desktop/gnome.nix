8ff345ecf207285c4197fa5a0cae4d6d563af4dc
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-01-26 06:57:15 +0100

    chore: atticd token

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -6,13 +6,11 @@
         gtk4
         gtk4-layer-shell
         gnome-keyring
-        gnome-extension-manager
         gnome-tweaks
         gnome-themes-extra
         gnome-calculator
         gnomeExtensions.appindicator
         gnomeExtensions.blur-my-shell
-        gnomeExtensions.home-assistant-extension
         gnomeExtensions.mock-tray
         gucharmap
         networkmanagerapplet
