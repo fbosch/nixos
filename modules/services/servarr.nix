2d61f7ef422cb3087662685547a20aa4e4de78f3
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-01-26 12:07:36 +0100

    feat: add media group and assign to servarr users

--- modules/services/servarr.nix
+++ modules/services/servarr.nix
@@ -4,6 +4,28 @@
     { lib, ... }:
     {
       config = {
+        users.groups.media = { };
+        users.users.bazarr.extraGroups = [
+          "media"
+          "users"
+        ];
+        users.users.jackett.extraGroups = [
+          "media"
+          "users"
+        ];
+        users.users.lidarr.extraGroups = [
+          "media"
+          "users"
+        ];
+        users.users.radarr.extraGroups = [
+          "media"
+          "users"
+        ];
+        users.users.sonarr.extraGroups = [
+          "media"
+          "users"
+        ];
+
         services.lidarr = {
           enable = true;
           openFirewall = lib.mkDefault true;
