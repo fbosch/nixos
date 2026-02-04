29a43d11a056569da51c40eced972355a452bb55
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-02-04 21:37:33 +0100

    feat(gh): add mcp and markdown extensions

--- modules/development/git.nix
+++ modules/development/git.nix
@@ -2,6 +2,21 @@
 {
   flake.modules.homeManager.development =
     { pkgs, ... }:
+    let
+      gh-mcp = pkgs.stdenvNoCC.mkDerivation {
+        pname = "gh-mcp";
+        version = "1.16.0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/shuymn/gh-mcp/releases/download/v1.16.0/linux-amd64";
+          hash = "sha256-51h6H990TFZlv5G22JPLwAS7mPObPdQqMbM83cZVOhg=";
+        };
+        dontUnpack = true;
+        installPhase = ''
+          install -Dm755 "$src" "$out/bin/gh-mcp"
+          install -Dm755 "$src" "$out/share/gh/extensions/gh-mcp/gh-mcp"
+        '';
+      };
+    in
     {
       home.packages = with pkgs; [
         # git-credential-manager
@@ -9,7 +24,6 @@
         delta
         difftastic
         gitui
-        gh
       ];
 
       programs.git = {
@@ -19,6 +33,14 @@
           "https://github.com".username = config.flake.meta.user.github.username;
           credentialStore = "secretservice";
         };
+      };
+
+      programs.gh = {
+        enable = true;
+        extensions = with pkgs; [
+          gh-mcp
+          gh-markdown-preview
+        ];
       };
     };
 }
