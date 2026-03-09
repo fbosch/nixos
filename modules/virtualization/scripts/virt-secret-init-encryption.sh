c966617974e87541cf4d97ab9c1c5ad0b3d7824e
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-03-09 10:22:34 +0100

    chore: lint

--- modules/virtualization/scripts/virt-secret-init-encryption.sh
+++ modules/virtualization/scripts/virt-secret-init-encryption.sh
@@ -6,4 +6,4 @@ readonly secrets_encryption_key_path="/var/lib/libvirt/secrets/secrets-encryptio
 umask 0077
 
 dd if=/dev/random status=none bs=32 count=1 |
-  systemd-creds encrypt --name=secrets-encryption-key - "$secrets_encryption_key_path"
+ systemd-creds encrypt --name=secrets-encryption-key - "$secrets_encryption_key_path"
