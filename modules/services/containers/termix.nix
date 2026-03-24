3a03e554b4fe149b012e651a5d705ac69e40f9f5
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-03-24 10:14:15 +0100

    style(termix): reformat nix module & bump image

--- modules/services/containers/termix.nix
+++ modules/services/containers/termix.nix
@@ -28,7 +28,7 @@ _: {
 
           [Container]
           ContainerName=termix
-          Image=ghcr.io/lukegus/termix:release-1.11.1
+          Image=ghcr.io/lukegus/termix:release-2.0.0
           PublishPort=${toString config.services.termix-container.port}:8080
           Volume=termix-data.volume:/app/data
           Environment=PORT=8080
