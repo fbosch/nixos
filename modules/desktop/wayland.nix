47e2a11d2466ab7a10c090197ad705a6148241c0
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-04-04 15:55:47 +0200

    chore: update

--- modules/desktop/wayland.nix
+++ modules/desktop/wayland.nix
@@ -34,6 +34,7 @@
           pkgs.wlr-randr
           pkgs.wl-clipboard
           pkgs.wl-clipboard-x11
+          pkgs.autocutsel
           pkgs.cliphist
           pkgs.wl-clip-persist
           pkgs.waybar
@@ -44,37 +45,77 @@
         ]
       );
 
-      systemd.user.services.cliphist = {
-        Unit = {
-          Description = "Clipboard history service for Wayland";
-          PartOf = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
-        };
+      systemd = {
+        user = {
+          services = {
+            cliphist = {
+              Unit = {
+                Description = "Clipboard history service for Wayland";
+                PartOf = [ "graphical-session.target" ];
+                After = [ "graphical-session.target" ];
+              };
 
-        Service = {
-          ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store";
-          Restart = "on-failure";
-        };
+              Service = {
+                ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store";
+                Restart = "on-failure";
+              };
 
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
-      };
+              Install = {
+                WantedBy = [ "graphical-session.target" ];
+              };
+            };
 
-      systemd.user.services.wl-clip-persist = {
-        Unit = {
-          Description = "Persist Wayland clipboard after programs exit";
-          PartOf = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
-        };
+            wl-clip-persist = {
+              Unit = {
+                Description = "Persist Wayland clipboard after programs exit";
+                PartOf = [ "graphical-session.target" ];
+                After = [ "graphical-session.target" ];
+              };
 
-        Service = {
-          ExecStart = "${pkgs.wl-clip-persist}/bin/wl-clip-persist --clipboard regular";
-          Restart = "on-failure";
-        };
+              Service = {
+                ExecStart = "${pkgs.wl-clip-persist}/bin/wl-clip-persist --clipboard regular";
+                Restart = "on-failure";
+              };
 
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
+              Install = {
+                WantedBy = [ "graphical-session.target" ];
+              };
+            };
+
+            autocutsel-clipboard = {
+              Unit = {
+                Description = "Sync X11 CLIPBOARD with PRIMARY";
+                PartOf = [ "graphical-session.target" ];
+                After = [ "graphical-session.target" ];
+              };
+
+              Service = {
+                ExecStart = "${pkgs.autocutsel}/bin/autocutsel -selection CLIPBOARD";
+                Restart = "on-failure";
+              };
+
+              Install = {
+                WantedBy = [ "graphical-session.target" ];
+              };
+            };
+
+            autocutsel-primary = {
+              Unit = {
+                Description = "Sync X11 PRIMARY with CLIPBOARD";
+                PartOf = [ "graphical-session.target" ];
+                After = [ "graphical-session.target" ];
+              };
+
+              Service = {
+                ExecStart = "${pkgs.autocutsel}/bin/autocutsel -selection PRIMARY";
+                Restart = "on-failure";
+              };
+
+              Install = {
+                WantedBy = [ "graphical-session.target" ];
+              };
+            };
+          };
         };
       };
     };
