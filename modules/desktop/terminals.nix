4f7a48cf9b122ef51b958ba8255fe5c10bfa1c36
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-04-14 10:53:00 +0200

    refactor(desktop): drop stable pkgs import

--- modules/desktop/terminals.nix
+++ modules/desktop/terminals.nix
@@ -1,13 +1,7 @@
-{ inputs, ... }:
+_:
 {
   flake.modules.homeManager.desktop =
     { pkgs, ... }:
-    let
-      pkgsStable = import inputs.nixpkgs-stable {
-        inherit (pkgs.stdenv.hostPlatform) system;
-        config.allowUnfree = true;
-      };
-    in
     {
       home.packages = [
         pkgs.wezterm
