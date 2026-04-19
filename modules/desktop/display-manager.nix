7d6e643439c3cd03560c2bde1bf8964e045361c6
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-04-19 19:33:43 +0200

    feat(desktop): show kernel on lock screen

--- modules/desktop/display-manager.nix
+++ modules/desktop/display-manager.nix
@@ -1,103 +1,121 @@
-{ inputs, ... }:
+{ inputs
+, ...
+}:
 let
   sddmBackground = ../../assets/wallpaper.png;
 in
 {
-  flake.modules.nixos.desktop = {
-    imports = [ inputs.silentSDDM.nixosModules.default ];
+  flake.modules.nixos.desktop =
+    { config, ... }:
+    let
+      nixosVersion = config.system.nixos.release;
+      linuxKernelVersion = config.boot.kernelPackages.kernel.version;
+    in
+    {
+      imports = [ inputs.silentSDDM.nixosModules.default ];
 
-    programs.silentSDDM = {
-      enable = true;
-      theme = "default";
-      backgrounds = {
-        main = sddmBackground;
-      };
-      settings = {
-        General = {
-          scale = 1.0;
-          enable-animations = true;
-        };
-        LockScreen = {
-          background = "wallpaper.png";
-          blur = 7;
-        };
-        "LockScreen.Clock" = {
-          "font-family" = "\"SF Pro Rounded\"";
-          format = "HH:mm";
-          "font-size" = 140;
-          "font-weight" = 700;
-          color = "#E6FFFFFF";
-        };
-        "LockScreen.Date" = {
-          "font-family" = "\"SF Pro Rounded\"";
-          format = "dddd, dd MMMM";
-          locale = "en_US";
-          "font-size" = 23;
-          "font-weight" = 500;
-          color = "#B3FFFFFF";
-          "margin-top" = -30;
-        };
-        LoginScreen = {
-          background = "wallpaper.png";
-          use-background-color = false;
-          background-color = "#000000";
-          blur = 7;
-        };
-        "LoginScreen.LoginArea" = {
-          position = "center";
-          margin = -1;
-        };
-        "LoginScreen.LoginArea.Avatar" = {
-          shape = "circle";
-          "active-size" = 80;
-          "inactive-size" = 80;
-          "inactive-opacity" = 1.0;
-        };
-        "LoginScreen.LoginArea.PasswordInput" = {
-          width = 230;
-          height = 43;
-          border-size = 0;
-          "border-radius-left" = 22;
-          "border-radius-right" = 22;
-          "background-opacity" = 0.12;
-          "content-color" = "#BFFFFFFF";
-          "font-size" = 18;
-          "font-family" = "\"SF Pro Rounded\"";
-        };
-        "LoginScreen.LoginArea.Username" = {
-          "font-family" = "\"SF Pro Rounded\"";
-          "font-size" = 23;
-          color = "#B3FFFFFF";
-        };
-        "LoginScreen.LoginArea.LoginButton" = {
-          "font-family" = "\"SF Pro Rounded\"";
-          "font-size" = 18;
-        };
-      };
-    };
-
-    services.displayManager = {
-      ly.enable = false;
-      sddm = {
-        wayland = {
-          enable = true;
-          compositor = "weston";
+      programs.silentSDDM = {
+        enable = true;
+        theme = "default";
+        backgrounds = {
+          main = sddmBackground;
         };
         settings = {
-          Theme = {
-
-            EnableAvatars = true;
-            FacesDir = "/etc/sddm/faces";
+          General = {
+            scale = 1.0;
+            enable-animations = true;
+          };
+          LockScreen = {
+            background = "wallpaper.png";
+            blur = 7;
+          };
+          "LockScreen.Clock" = {
+            "font-family" = "\"SF Pro Rounded\"";
+            format = "HH:mm";
+            "font-size" = 140;
+            "font-weight" = 700;
+            color = "#E6FFFFFF";
+          };
+          "LockScreen.Date" = {
+            "font-family" = "\"SF Pro Rounded\"";
+            format = "dddd, dd MMMM";
+            locale = "en_US";
+            "font-size" = 23;
+            "font-weight" = 500;
+            color = "#B3FFFFFF";
+            "margin-top" = -30;
+          };
+          "LockScreen.Message" = {
+            "display-icon" = false;
+            text = "NixOS ${nixosVersion} | Linux ${linuxKernelVersion}\nPress any key";
+            position = "bottom-center";
+            "font-family" = "\"SF Pro Rounded\"";
+            "font-size" = 14;
+            "font-weight" = 500;
+            color = "#B3FFFFFF";
+            spacing = 6;
+          };
+          LoginScreen = {
+            background = "wallpaper.png";
+            use-background-color = false;
+            background-color = "#000000";
+            blur = 7;
+          };
+          "LoginScreen.LoginArea" = {
+            position = "center";
+            margin = -1;
+          };
+          "LoginScreen.LoginArea.Avatar" = {
+            shape = "circle";
+            "active-size" = 80;
+            "inactive-size" = 80;
+            "inactive-opacity" = 1.0;
+          };
+          "LoginScreen.LoginArea.PasswordInput" = {
+            width = 230;
+            height = 43;
+            border-size = 0;
+            "border-radius-left" = 22;
+            "border-radius-right" = 22;
+            "background-opacity" = 0.12;
+            "content-color" = "#BFFFFFFF";
+            "font-size" = 18;
+            "font-family" = "\"SF Pro Rounded\"";
+          };
+          "LoginScreen.LoginArea.Username" = {
+            "font-family" = "\"SF Pro Rounded\"";
+            "font-size" = 23;
+            color = "#B3FFFFFF";
+          };
+          "LoginScreen.LoginArea.LoginButton" = {
+            "font-family" = "\"SF Pro Rounded\"";
+            "font-size" = 18;
           };
         };
       };
-      defaultSession = "hyprland-uwsm";
-    };
 
-    systemd.services.display-manager.environment = {
-      XCURSOR_THEME = "WinSur-white-cursors";
-      XCURSOR_SIZE = "28";
-      XCURSOR_PATH = "/run/current-system/sw/share/icons";
+      services.displayManager = {
+        ly.enable = false;
+        sddm = {
+          wayland = {
+            enable = true;
+            compositor = "weston";
+          };
+          settings = {
+            Theme = {
+
+              EnableAvatars = true;
+              FacesDir = "/etc/sddm/faces";
+            };
+          };
+        };
+        defaultSession = "hyprland-uwsm";
+      };
+
+      systemd.services.display-manager.environment = {
+        XCURSOR_THEME = "WinSur-white-cursors";
+        XCURSOR_SIZE = "28";
+        XCURSOR_PATH = "/run/current-system/sw/share/icons";
+      };
     };
-  };
 }
