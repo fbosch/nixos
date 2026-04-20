4c85987133e36810006eac9297f44ad0eb28edfe
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-04-20 07:16:45 +0200

    fix(storage): use ntfs driver

--- modules/hosts/rvn-pc/storage.nix
+++ modules/hosts/rvn-pc/storage.nix
@@ -8,14 +8,13 @@
       # Mount 2TB HDD (shared with Windows)
       "/mnt/storage" = {
         device = "/dev/disk/by-uuid/AC7674097673D316";
-        fsType = "ntfs-3g";
+        fsType = "ntfs";
         options = [
           "rw" # Read-write access
           "uid=1000" # Owner UID (your user)
           "gid=100" # Group GID (users group)
           "dmask=022" # Directory permissions (755)
           "fmask=022" # File permissions (755) - allows execution
-          "big_writes" # Better write performance
           "noatime" # Don't update access times (better performance)
           "nofail" # Do not block boot if the disk is unavailable
           "x-systemd.automount" # Mount on first access instead of during boot
@@ -25,14 +24,13 @@
       # Mount Games SSD
       "/mnt/games" = {
         device = "/dev/disk/by-uuid/B86CB0876CB04244";
-        fsType = "ntfs-3g";
+        fsType = "ntfs";
         options = [
           "rw" # Read-write access
           "uid=1000" # Owner UID (your user)
           "gid=100" # Group GID (users group)
           "dmask=022" # Directory permissions (755)
           "fmask=022" # File permissions (755) - allows execution
-          "big_writes" # Better write performance
           "noatime" # Don't update access times (better performance)
           "nofail" # Do not block boot if the disk is unavailable
           "x-systemd.automount" # Mount on first access instead of during boot
