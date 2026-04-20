d781fcb19a9a2c2e74e3d2cff51bd0a122080bdf
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-04-20 13:41:52 +0200

    feat(shell): add nix-index shell module

--- flake.nix
+++ flake.nix
@@ -24,6 +24,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
