3d9678686eb9079bcdbe486f671e5eae9c08f0e7
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-05-07 19:21:03 +0200

    fix(nemo): use wezterm wrapper for nemo

--- modules/applications/nemo/preferences.nix
+++ modules/applications/nemo/preferences.nix
@@ -1,27 +1,42 @@
 {
-  flake.modules.homeManager.applications = _: {
-    dconf.settings = {
-      "org/nemo/sidebar-panels/tree" = {
-        sync-bookmarks = true;
+  flake.modules.homeManager.applications =
+    { pkgs, ... }:
+    let
+      weztermForNemo = pkgs.writeShellApplication {
+        name = "nemo-wezterm";
+        runtimeInputs = [ pkgs.wezterm ];
+        text = ''
+          if [ "$#" -gt 0 ] && [ "$1" = "--" ]; then
+            shift
+          fi
+
+          exec wezterm start --cwd "$PWD" "$@"
+        '';
       };
-      "org/cinnamon/desktop/applications/terminal" = {
-        exec = "wezterm";
-        exec-arg = "start --cwd";
-      };
-      "org/nemo/preferences" = {
-        enable-delete = true;
-        show-delete-permanently = true;
-        show-location-entry = true;
-        mouse-use-extra-buttons = false;
-        show-image-thumbnails = "always";
-        default-folder-viewer = "icon-view";
-        show-hidden-files = true;
-        show-full-path-titles = true;
-        quick-renames-with-pause-in-between = true;
-      };
-      "org/nemo/window-state" = {
-        start-with-sidebar = true;
+    in
+    {
+      dconf.settings = {
+        "org/nemo/sidebar-panels/tree" = {
+          sync-bookmarks = true;
+        };
+        "org/cinnamon/desktop/applications/terminal" = {
+          exec = "${weztermForNemo}/bin/nemo-wezterm";
+          exec-arg = "--";
+        };
+        "org/nemo/preferences" = {
+          enable-delete = true;
+          show-delete-permanently = true;
+          show-location-entry = true;
+          mouse-use-extra-buttons = false;
+          show-image-thumbnails = "always";
+          default-folder-viewer = "icon-view";
+          show-hidden-files = true;
+          show-full-path-titles = true;
+          quick-renames-with-pause-in-between = true;
+        };
+        "org/nemo/window-state" = {
+          start-with-sidebar = true;
+        };
       };
     };
-  };
 }
