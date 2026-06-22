f4d35c74174322dfc872ff5c01ce23d07e6cf910
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-06-22 16:28:06 +0200

    fix(vicinae): start vicinae with session

--- modules/applications/vicinae.nix
+++ modules/applications/vicinae.nix
@@ -2,5 +2,7 @@
   flake.modules.nixos.applications = { pkgs, ... }: {
     environment.systemPackages = [ pkgs.vicinae ];
     systemd.packages = [ pkgs.vicinae ];
+
+    systemd.user.targets.graphical-session.wants = [ "vicinae.service" ];
   };
 }
