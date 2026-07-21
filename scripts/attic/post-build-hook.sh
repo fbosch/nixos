ef0165a7ba98e48f28aabe6830a868670ac850cf
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-07-21 21:38:03 +0200

    fix(scripts): limit attic push jobs

--- scripts/attic/post-build-hook.sh
+++ scripts/attic/post-build-hook.sh
@@ -17,7 +17,7 @@ fi
 
 if "$attic_cli" login --set-default rvn "$endpoint" "$(cat "$token_file")" >/dev/null 2>&1; then
   IFS=' ' read -r -a out_paths <<<"${OUT_PATHS}"
-  printf '%s\n' "${out_paths[@]}" | "$attic_cli" push "$cache_name" --stdin >/dev/null 2>&1 ||
+  printf '%s\n' "${out_paths[@]}" | "$attic_cli" push "$cache_name" --jobs 1 --stdin >/dev/null 2>&1 ||
     echo "attic post-build hook: push failed" >&2
 else
   echo "attic post-build hook: login failed" >&2
