abdd681f8bf0933deb93bd003b45c4d592c9f11e
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-07-27 07:29:46 +0200

    fix(nixpkgs): update lact and drop workaround

--- modules/applications/gaming/lact.nix
+++ modules/applications/gaming/lact.nix
@@ -1,10 +1,15 @@
 _: {
   flake.modules.nixos.gaming =
     { pkgs, ... }:
+    let
+      lact = pkgs.lact.override {
+        libdisplay-info = pkgs.libdisplay-info_0_2;
+      };
+    in
     {
-      environment.systemPackages = [ pkgs.lact ];
+      environment.systemPackages = [ lact ];
 
-      systemd.packages = [ pkgs.lact ];
+      systemd.packages = [ lact ];
       systemd.services.lactd = {
         enable = true;
         wantedBy = [ "multi-user.target" ];
