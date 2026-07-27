b4cda1e2b38d29734a28b3c1d915a3cc504f1bcf
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-07-27 22:07:39 +0200

    chore: update helium

--- pkgs/by-name/helium-browser/package.nix
+++ pkgs/by-name/helium-browser/package.nix
@@ -7,11 +7,11 @@
 
 let
   pname = "helium-browser";
-  version = "0.14.5.1";
+  version = "0.14.9.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-JM4Tm4Le9Xcfq3fFMEu/DIK6817FEgBQ2rSwY093F04=";
+    hash = "sha256-cuQiMGhOPjE7ixuZiFGpRuGF9SdVcNPYUXSXhjZBLKQ=";
   };
 
   icon = fetchurl {
