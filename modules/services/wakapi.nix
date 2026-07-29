beb343620639fa81c19aeffb390374d31c5287b5
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-07-29 18:50:45 +0200

    feat(wakapi): use secretspec for wakapi

--- modules/services/wakapi.nix
+++ modules/services/wakapi.nix
@@ -1,4 +1,7 @@
-{ config, ... }:
+{ inputs
+, config
+, ...
+}:
 let
   inherit (config.flake.lib) sopsHelpers;
 in
@@ -6,10 +9,16 @@ in
   flake.modules.nixos."services/wakapi" =
     { config
     , lib
+    , pkgs
     , ...
     }:
     let
       port = 3033;
+      secretspec = inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.secretspec;
+      secretspecManifest = pkgs.writeText "secretspec.toml" (builtins.readFile ../../secretspec.toml);
+      wakapiSettingsFile =
+        (pkgs.formats.yaml { }).generate "wakapi-settings"
+          config.services.wakapi.settings;
     in
     {
       config = lib.mkMerge [
@@ -40,21 +49,21 @@ in
           };
         }
         (lib.mkIf (config ? sops) {
-          sops.secrets.wakapi-password-salt = sopsHelpers.mkSecret ../../secrets/apis.yaml {
-            mode = lib.mkDefault sopsHelpers.wheelReadable.mode;
-            group = lib.mkDefault sopsHelpers.wheelReadable.group;
-          };
+          sops.secrets.wakapi-password-salt = sopsHelpers.mkSecret ../../secrets/apis.yaml sopsHelpers.rootOnly;
 
-          sops.templates."wakapi-env" = {
-            content = ''
-              WAKAPI_PASSWORD_SALT=${config.sops.placeholder.wakapi-password-salt}
+          systemd.services.wakapi = {
+            after = [ "sops-install-secrets.service" ];
+            wants = [ "sops-install-secrets.service" ];
+            # The upstream module supports EnvironmentFile but not a credential-aware command.
+            script = lib.mkForce ''
+              exec ${lib.getExe secretspec} --file ${secretspecManifest} run --profile systemd --scope wakapi --reason "Start Wakapi" -- ${lib.getExe config.services.wakapi.package} -config ${wakapiSettingsFile}
             '';
-            mode = "0400";
+            serviceConfig = {
+              LoadCredential = [
+                "WAKAPI_PASSWORD_SALT:${config.sops.secrets.wakapi-password-salt.path}"
+              ];
+            };
           };
-
-          services.wakapi.environmentFiles = [
-            config.sops.templates."wakapi-env".path
-          ];
         })
         {
           networking.firewall.allowedTCPPorts = lib.mkAfter [ port ];
