0f3d64977065f7c2bdff8935f98c5dc7d1ae456a
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-09-05 08:37:33 +0200

    fix(storage): use ext4 games drive

--- modules/hosts/rvn-pc/storage.nix
+++ modules/hosts/rvn-pc/storage.nix
@@ -22,16 +22,12 @@
         ];
       };
 
-      # Mount Games SSD
+      # Mount Linux-only Games SSD
       "/mnt/games" = {
-        device = "/dev/disk/by-uuid/B86CB0876CB04244";
-        fsType = "ntfs";
+        device = "/dev/disk/by-uuid/ea2a9163-46aa-4c77-b044-359619f50cb1";
+        fsType = "ext4";
         options = [
           "rw" # Read-write access
-          "uid=1000" # Owner UID (your user)
-          "gid=100" # Group GID (users group)
-          "dmask=022" # Directory permissions (755)
-          "fmask=022" # File permissions (755) - allows execution
           "noatime" # Don't update access times (better performance)
           "nofail" # Do not block boot if the disk is unavailable
           "x-systemd.automount" # Mount on first access instead of during boot
